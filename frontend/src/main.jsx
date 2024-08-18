0b5ad192e5aa4f8b26d6fe1a8fa1c34591ab9b2e
Arjun Bhandari <arjun12345bhandari@gmail.com>
2024-08-18 19:22:39 +0530

    File structure organize

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,7 @@ import { Provider } from 'react-redux'
 import { RouterProvider,createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import Home from './pages/Home.jsx'
-import Login from './pages/Login.jsx'
-import Signup from './pages/Signup.jsx'
-
+import {Home,Login,Signup} from "./pages/index.js"
 const router = createBrowserRouter([
   {
     path:"/",
